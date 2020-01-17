457ff30784bf6c6e298295f34e0cba31eddd5332
Sandesh Suvarna <sandesh_suvarna@colpal.com>
2020-01-17 18:13:10 +0530

    Django Polls part 2

--- Django Web Project/mysite/polls/admin.py
+++ Django Web Project/mysite/polls/admin.py
@@ -4,6 +4,26 @@ from .models import Question
 from .models import Choice
 # Register your models here.
 
-admin.site.register(Question)
+#admin.site.register(Question)
 
-admin.site.register(Choice)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+# admin.site.register(Question, QuestionAdmin)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
